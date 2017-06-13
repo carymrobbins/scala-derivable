e278650ffc366fd7c1d5fed1352afa8cb0f7315d
Cary Robbins <carymrobbins@gmail.com>
2017-06-12 19:40:22 -0500

    Added TODO failing test cases

--- tests/src/main/scala/io/estatico/test/CsvEncoder.scala
+++ tests/src/main/scala/io/estatico/test/CsvEncoder.scala
@@ -10,6 +10,8 @@ object CsvEncoder {
 
   def apply[A](implicit ev: CsvEncoder[A]): CsvEncoder[A] = ev
 
+  def encode[A](a: A)(implicit ev: CsvEncoder[A]): String = ev.encode(a)
+
   def instance[A](f: A => String): CsvEncoder[A] = new CsvEncoder[A] {
     override def encode(a: A): String = f(a)
   }
@@ -19,11 +21,11 @@ object CsvEncoder {
   // Cached instance for all fromToString instances.
   private val _fromToString = CsvEncoder.instance[Any](_.toString)
 
-  def derive[C]: CsvEncoderDeriver[C] = new CsvEncoderDeriver[C]
-
   implicit val int: CsvEncoder[Int] = CsvEncoder.fromToString
 
   implicit val float: CsvEncoder[Float] = CsvEncoder.fromToString
+
+  implicit val double: CsvEncoder[Double] = CsvEncoder.fromToString
 
   implicit val string: CsvEncoder[String] = CsvEncoder.instance(s =>
     if (s.contains(',')) '"' + s.replace("\"", "\"\"") + '"' else s
@@ -31,16 +33,24 @@ object CsvEncoder {
 
   implicit def gFields1[A](implicit aEnc: CsvEncoder[A]): CsvEncoder[GenericFields._1[A]]
     = CsvEncoder.instance(a => aEnc.encode(GenericList.head(a)))
+
+  implicit def generic[A, H, T](
+    implicit
+    gl: GenericList[A, H, T],
+    hEnc: CsvEncoder[H],
+    tEnc: CsvEncoder[T]
+  ): CsvEncoder[A] = CsvEncoder.instance(a =>
+    hEnc.encode(gl.head(a)) + ',' + tEnc.encode(gl.tail(a))
+  )
+
+  implicit def derive[C] = new CsvEncoderDeriver[C]
 }
 
-final class CsvEncoderDeriver[C]
-object CsvEncoderDeriver {
-  implicit def derive[C, H, T](c: CsvEncoderDeriver[C])(
+final class CsvEncoderDeriver[C] {
+  def apply[H, T]()(
     implicit
     gl: GenericList[C, H, T],
     hEnc: CsvEncoder[H],
     tEnc: CsvEncoder[T]
-  ): CsvEncoder[C] = CsvEncoder.instance(a =>
-    hEnc.encode(gl.head(a)) + ',' + tEnc.encode(gl.tail(a))
-  )
+  ): CsvEncoder[C] = CsvEncoder.generic[C, H, T]
 }
--- tests/src/test/scala/io/estatico/generic/GenericProductTest.scala
+++ tests/src/test/scala/io/estatico/generic/GenericProductTest.scala
@@ -8,14 +8,34 @@ class GenericProductTest extends FlatSpec with Matchers {
   import GenericProductTest._
 
   "CsvEncoder" should "be derivable" in {
-    CsvEncoder[ExampleCase].encode(ExampleCase("foo", 3)) shouldEqual "foo,3"
+    //TODO
+    //CsvEncoder.encode(Example1("blah")) shouldEqual "blah"
+    CsvEncoder.encode(Example2("foo", 3)) shouldEqual "foo,3"
+    CsvEncoder.encode(Example3("hey", 4, 5.2f)) shouldEqual "hey,4,5.2"
+    CsvEncoder.encode(Example4("ya", 2, 8.2f, 7.6)) shouldEqual "ya,2,8.2,7.6"
   }
+
+  //TODO
+  //it should "not auto-derive" in {
+  //  assertCompiles("CsvEncoder[Example2]")
+  //  assertDoesNotCompile("CsvEncoder[NoExample2]")
+  //}
 }
 
 object GenericProductTest {
 
-  @GenericProduct
-  case class ExampleCase(a: String, b: Int)
+  //TODO
+  //@GenericProduct case class Example1(a: String)
+  //implicit val csvEncEx1: CsvEncoder[Example1] = CsvEncoder.generic
 
-  implicit val csvEncEx: CsvEncoder[ExampleCase] = CsvEncoder.derive[ExampleCase]
+  @GenericProduct case class Example2(a: String, b: Int)
+  implicit val csvEncEx2: CsvEncoder[Example2] = CsvEncoder.generic
+
+  @GenericProduct case class Example3(a: String, b: Int, c: Float)
+  implicit val csvEncEx3: CsvEncoder[Example3] = CsvEncoder.generic
+
+  @GenericProduct case class Example4(a: String, b: Int, c: Float, d: Double)
+  implicit val csvEncEx4: CsvEncoder[Example4] = CsvEncoder.generic
+
+  @GenericProduct case class NoExample2(a: String, b: Int)
 }
